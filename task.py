e7eeb2b570b8818339920a32f02fa5b257e34f04
Vahur Roosimaa <vahur@scopely.com>
2012-11-05 00:07:45 -0800

    Simple server-side task parsing and decorating

--- task.py
+++ task.py
@@ -4,6 +4,7 @@ from server import app, mongo
 @app.route("/task/create", methods=["POST"])
 def task_create():
     if is_task_valid(request.json):
+        decorate_task(request.json)
         mongo.db.Tasks.insert(request.json)
         del request.json["_id"]
         return jsonify(request.json)
@@ -12,3 +13,27 @@ def task_create():
 
 def is_task_valid(taskJSON):
     return True
+
+def decorate_task(task):
+    result = task["description"];
+
+    # Loop through the tags array backwards to prevent replacements from changing the locations
+    for tag in reversed(task["tags"]):
+        if tag["type"] == 3:
+            continue;
+
+        prefix = result[0: tag["location"]]
+        name = result[tag["location"]: tag["location"] + len(tag["value"]) + 1]
+        suffix = result[tag["location"] + len(tag["value"]) + 1:]
+
+        if tag["action"] == "!":
+            className = "notify"
+        elif tag["action"] == "@":
+            className = "assign"
+        else:
+            className = "categorize"
+
+        result = "".join([prefix, "<span class=\"", className, "\">", name, "</span>", suffix]);
+
+    task["decorated"] = result
+    print(result)
